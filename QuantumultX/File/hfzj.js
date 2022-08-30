d01a45c6a34eb5b5959758e0ba85bc92835f5492
Musk.Chen <Musk.Chen@fanruan.com>
2022-08-30 12:35:54 +0800

    fix

--- QuantumultX/File/hfzj.js
+++ QuantumultX/File/hfzj.js
@@ -3,7 +3,7 @@
 QuantumultX:
 
 [rewrite_local]
-^https?:\/\/gw-app\.beantechyun\.com\/app-api\/api\/v1.0\/vehicle\/getVehicleService url script-response-body https://raw.githubusercontent.com/gcdd1993/Script/master/QuantumultX/File/hfzj.js
+^https?:\/\/gw-app\.beantechyun\.com\/app-api\/api\/v1.0\/vehicle\/getVehicleService\?vin=.* url script-response-body https://raw.githubusercontent.com/gcdd1993/Script/master/QuantumultX/File/hfzj.js
 
 [mitm]
 hostname = gw-app.beantechyun.com
@@ -11,7 +11,7 @@ hostname = gw-app.beantechyun.com
 **************************/
 let body = $response.body;
 const obj = JSON.parse(body);
-obj.data.forEach(item => {
+obj.data.items.forEach(item => {
     item.state = 1
 })
 
